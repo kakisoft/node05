cac5667ef2c578b14a30c1b97f62f7d7d8835b99
kakisoft <kakisttab@gmail.com>
2017-05-10 01:41:46 +0900

    2nd commit

--- Sample01.js
+++ Sample01.js
@@ -6,6 +6,12 @@ var ejs = require('ejs');
 var index = fs.readFileSync('./index.ejs','utf8');
 var style = fs.readFileSync('./style.css','utf8');
 
+var datas = [
+    {'name':'Taro','mail':'taro@yamada','tel':'090-9999-9999'},
+    {'name':'hanako','mail':'hanako@flower','tel':'090-8888-8888'},
+    {'name':'ichiro','mail':'ichiro@baseball','tel':'090-7777-7777'}
+];
+
 var server = http.createServer();
 server.on('request',doRequest);
 server.listen(1337);
@@ -17,7 +23,8 @@ function doRequest(req, res){
         case '/':
             var tmp = ejs.render(index,{
                 title:"Index Page",
-                msg:"This is Sample Page"
+                msg:"This is Sample Page",
+                datas:datas
             });
             res.setHeader('Content-Type','text/html');
             res.write(tmp);
